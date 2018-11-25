52f5bd88a814a020fcb31f3e48a604c11866fcff
Soronne <aceforu@hotmail.fr>
2018-11-25 07:25:02 +0100

    FinalOne

--- UPEMCorp/src/IStatus.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IStatus extends Remote{
-	public int getMark() throws RemoteException;
-	public void setMark(int mark) throws RemoteException;
-	public String getDescription() throws RemoteException;
-	public void setDescription(String description) throws RemoteException;
-}
