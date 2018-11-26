18c89de584af99781518d901ab4cf3d776846ebd
LyZeon <delver.jordan@gmail.com>
2018-11-26 02:33:08 +0100

    rendu

--- RentCarsUPEM/src/buy/ICarService.java
+++
@@ -1,9 +0,0 @@
-package buy;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ICarService extends Remote{
-	public boolean buyCar(int carID,String email,String password) throws RemoteException;
-	public ICar[] getCars() throws RemoteException;
-}
